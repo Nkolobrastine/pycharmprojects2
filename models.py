56e352ed9bb35d6df939d62742c3dd5d78d211d8
nkolobrastine <nkolobrastine8@gmail.com>
2021-09-10 16:46:25 +0100

    third commit

---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String,DateTime
+from sqlalchemy.orm import relationship
+import datetime
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    created_date = Column(DateTime,default=datetime.datetime.utcnow)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean,default=True)
+    post = relationship("Post", back_populates="owner")
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    created_date = Column(DateTime,default=datetime.datetime.utcnow)
+    is_active = Column(Boolean, default=True)
+    title = Column(String)
+    body = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="post")
+
---
+++ schemas.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from pydantic import BaseModel
+import datetime
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+class TokenData(BaseModel):
+    username: Optional[str] = None
+class User(BaseModel):
+    username: str
+class UserInDB(User):
+    hashed_password: str
+class UserCreate(User):
+    password: str
+
+class PostBase(BaseModel):
+    title:str
+
+class PostList(PostBase):
+    created_date: Optional[datetime.datetime]
+    owner_id: int
+    owner: User
+
+    class Config:
+        orm_mode=True
+
+
